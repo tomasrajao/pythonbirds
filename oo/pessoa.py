2767ba4c129229caee27f54c15c4187e4d734717
tomasrajao <tomasrajao@gmail.com>
2021-09-15 17:52:51 -0300

    Criado atributo complexo "filhos"

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,10 @@
 # Classe
 class Pessoa:
     # atributo de dado
-    def __init__(self, nome=None, idade=28):  # def __init__(self, parâmetro)
-        self.nome = nome # "atributo.de_dado = parâmetro
+    def __init__(self, *filhos, nome=None, idade=28):  # def __init__(self, parâmetro)
+        self.nome = nome  # "atributo.de_dado = parâmetro
         self.idade = idade
+        self.filhos = list(filhos)
 
     # Método (atributo da classe)
     def cumprimentar(self):
@@ -11,13 +12,18 @@ class Pessoa:
 
 
 if __name__ == '__main__':
-    p = Pessoa('Luciano')
+    renzo = Pessoa(idade=35)
+    tomas = Pessoa(idade=28)
+    cleber = Pessoa(idade=40)
+    luciano = Pessoa(renzo, tomas, cleber, nome='Luciano')
     # Chamando classe e método, precisa passar o objeto "p" (atributo da classe)
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
     # Contudo, é possível chamar diretamente o próprio objeto "p", sem precisar passar o objeto novamente como parâmetro
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Tomas'
-    print(p.nome)
-    print(p.idade)
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    luciano.nome = 'Tomas'
+    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.idade)
